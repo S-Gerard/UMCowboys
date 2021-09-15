6c7d44d3760c8faad1680be763c8731c849622eb
Michal Kowalski <michalk45@yahoo.com>
2021-09-15 15:25:22 -0600

    maybe final

--- group.py
+++ group.py
@@ -1,14 +1,15 @@
 
 def Michal():
  
-n = str(Michal Kowalski)
+        n = str("Michal Kowalski")
+        y = str ("junior")
+        m = str ("Computer Science")
+        h= str("reading")
+        c= str("Poland")
+        print(n)
+        print(y)
+        print(m)
+        print(h)
+        print(c)
 
-y = str (junior)
-m = str (Computer Science)
-h= str(reading)
-c= str(Poland)
-print(n)
-print(y)
-print(m)
-print(h)
-print(c)
+Michal()
